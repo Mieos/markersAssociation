3f9502f1676437393f6060690aa834ef48d8aa7d
Richard Modrzejewski <richard.modrzejewski@gmail.com>
2018-06-22 17:25:32 +0200

    Fix

--- utils/addAssociation.m
+++ utils/addAssociation.m
@@ -15,12 +15,21 @@ if pointCloudIndex_GT>0 && pointCloudIndex_Used>0
     idPc = X(pointCloudIndex_Used);
     if (idGT<=nP1 && idPc<=nP1) || (idGT>nP1 && idPc>nP1)
         
-        correspondanceGT=correspondances(1,:);
-        correspondanceU=correspondances(2,:);
-        findGT=find(correspondanceGT==pointCloudIndex_GT);
-        findU=find(correspondanceU==X(pointCloudIndex_Used));
-        [~,sizeGT]=size(findGT);
-        [~,sizeU]=size(findU);
+        clearvars correspondanceGT correspondanceU findGT findU;
+        
+        [~,sizeC]=size(correspondances);
+        
+        if (sizeC == 0)
+            sizeGT=0;
+            sizeU=0;
+        else 
+            
+            findGT=find(correspondances(1,:)==pointCloudIndex_GT);
+            findU=find(correspondances(2,:)==X(pointCloudIndex_Used));
+            [~,sizeGT]=size(findGT);
+            [~,sizeU]=size(findU);
+            
+        end
         
         if(sizeGT==0 && sizeU==0)
             new_c = [pointCloudIndex_GT;X(pointCloudIndex_Used)];
